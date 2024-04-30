60a847109ec850390978bc3fbfe2500afa7ff89e
mystekerinos <jordy.saintalbin@gmail.com>
2024-04-30 23:12:19 +0200

    refactorisation-navbar-files

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,17 +1,42 @@
 import React from "react";
-import Logo from "../../assets/images/argentBankLogo.png";
-import Links from "./link/Link";
-
+import LogOut from "../loginOut/LoginOut";
 import { Link } from "react-router-dom";
-const Navbar = () => {
+import argentBankLogo from "../../assets/images/argentBankLogo.png";
+
+const Navbar = ({ showLogout, displayName }) => {
+  const isLoggedIn = localStorage.getItem("jwtToken") !== null;
+
   return (
-    <nav className="nav">
-      <Link to="/">
-        <img src={Logo} alt="Argent Bank - logo" />
+    <nav className="main-nav">
+      <Link to="/" className="main-nav-logo">
+        <img
+          className="main-nav-logo-image"
+          src={argentBankLogo}
+          alt="Argent Bank Logo"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
       </Link>
-      <ul>
-        <Links />
-      </ul>
+      <div className="nav-item">
+        {showLogout && isLoggedIn ? (
+          <>
+            <div className="main-nav-item">
+              <i className="fa fa-user-circle"></i>
+              {displayName}
+            </div>
+            <LogOut />
+          </>
+        ) : (
+          <Link
+            className="main-nav-item"
+            to={isLoggedIn ? "/profile" : "/login"}
+          >
+            <div>
+              <i className="fa fa-user-circle"></i>
+              {isLoggedIn ? displayName : "Sign In"}
+            </div>
+          </Link>
+        )}
+      </div>
     </nav>
   );
 };
