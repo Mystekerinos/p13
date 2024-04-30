f732b9528d5f50363df1b7614ce33346446c9c19
mystekerinos <jordy.saintalbin@gmail.com>
2024-04-30 23:04:31 +0200

    refactored-app-javascript-files

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store/store";
-import Home from "./pages/home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/signIn/SignIn";
 import PageNotFound from "./pages/pageNotFound/PageNotFound";
-import Login from "./pages/signin/Signin";
-import User from "./pages/user/User";
-import "./App.css";
+import Home from "./pages/home/Home";
 
-function App() {
+const App = () => {
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/user" element={<User />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </Router>
-    </Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
