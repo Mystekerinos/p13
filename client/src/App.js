8e5471b9a8979181f6f24e341be48b34ba4ee544
mystekerinos <jordy.saintalbin@gmail.com>
2024-05-09 11:40:59 +0200

    install-provider-in-app

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,21 @@ import ProfilePage from "./pages/profilePage/ProfilePage";
 import Login from "./pages/login/Login";
 import PageNotFound from "./pages/pageNotFound/PageNotFound";
 import Home from "./pages/home/Home";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
